d1b3e3587cdacb30697f3cb3d896d8580bb30242
codeBlack5 <stevekoech8@gmail.com>
2022-11-20 19:58:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if (activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+        }
+        })
+})
